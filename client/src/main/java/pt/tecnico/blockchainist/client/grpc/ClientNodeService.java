c9f8fb408186ac5aba1e6d6a2ac636ff06cc37b1
Pedro Antunes <pedro.santos.antunes@tecnico.ulisboa.pt>
2026-02-27 12:26:10 +0000

    Client Node Service stub implementation

--- client/src/main/java/pt/tecnico/blockchainist/client/grpc/ClientNodeService.java
+++ client/src/main/java/pt/tecnico/blockchainist/client/grpc/ClientNodeService.java
@@ -1,7 +1,41 @@
 package pt.tecnico.blockchainist.client.grpc;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import pt.tecnico.blockchainist.contract.*;
+
 public class ClientNodeService {
-    public ClientNodeService(String host, int port, String organization) {
+
+	private NodeServiceGrpc.NodeServiceBlockingStub stub;
+	private String organization;
+
+
+	public ClientNodeService(String host, int port, String organization) {
         // TODO: create channel/stub
+
+
+
+        final String target = host + ":" + port;
+
+		// Channel is the abstraction to connect to a service endpoint.
+		// Let us use plaintext communication because we do not have certificates.
+		final ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+
+
+
+		this.stub = NodeServiceGrpc.newBlockingStub(channel);
+		this.organization = organization;
+
     }
+
+	public String getOrganization(){
+		return this.organization;
+	}
+
+
+	public NodeServiceGrpc.NodeServiceBlockingStub getStub(){
+		return this.stub;
+	}
+
+
 }
