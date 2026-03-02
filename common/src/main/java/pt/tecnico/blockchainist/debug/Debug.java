83e2ece721bc3435105ef1e92db19331219707af
Michael Maycock <michael.maycock@tecnico.ulisboa.pt>
2026-03-02 19:40:16 +0000

    Add Debug class (-Ddebug=1)

---
+++ common/src/main/java/pt/tecnico/blockchainist/debug/Debug.java
@@ -0,0 +1,11 @@
+package pt.tecnico.blockchainist.debug;
+
+public class Debug {
+    private Debug() {}
+
+    public static final boolean ENABLED = "1".equals(System.getProperty("debug", "0"));
+
+    public static void log (String msg) {
+        if (ENABLED) System.out.println(msg);
+    }
+}
