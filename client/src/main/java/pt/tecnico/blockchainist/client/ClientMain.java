782f5675b6abf17ea170a5f15fe8dcdf9b80049a
Michael Maycock <michael.maycock@tecnico.ulisboa.pt>
2026-03-02 20:13:20 +0000

    Change logic to always execute node channel closure

--- client/src/main/java/pt/tecnico/blockchainist/client/ClientMain.java
+++ client/src/main/java/pt/tecnico/blockchainist/client/ClientMain.java
@@ -3,12 +3,15 @@ package pt.tecnico.blockchainist.client;
 import java.util.ArrayList;
 import pt.tecnico.blockchainist.client.grpc.ClientNodeService;
 
+import pt.tecnico.blockchainist.debug.Debug;
+
 
 public class ClientMain {
 
     public static void main(String[] args) {
 
         System.out.println(ClientMain.class.getSimpleName());
+        Debug.log("Debug is ON");
 
         // check arguments
         if (args.length < 1) {
@@ -19,36 +22,43 @@ public class ClientMain {
 
         // parse arguments
         ArrayList<ClientNodeService> nodes = new ArrayList<>(args.length);
-        for (String arg : args) {
-            String[] split = arg.split(":");
-            if (split.length != 3) {
-                System.err.println("Invalid argument: " + arg);
-                printUsage();
-                return;
-            }
-            String host = split[0];
-            int port = -1;
-            try {
-                port = Integer.parseInt(split[1]);
-            } catch (NumberFormatException e) {
-                System.err.println("Invalid port (" + split[1] + ") in argument: " + arg);
-                printUsage();
-                return;
-            }
-            if (port > 65535 || port < 0) {
-                System.err.println("Port number out of range (0-65535): " + port);
-                printUsage();
-                return;
-            }
-            String organization = split[2];
+        try {
+            for (String arg : args) {
+                String[] split = arg.split(":");
+                if (split.length != 3) {
+                    System.err.println("Invalid argument: " + arg);
+                    printUsage();
+                    return;
+                }
+                String host = split[0];
+                int port = -1;
+                try {
+                    port = Integer.parseInt(split[1]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Invalid port (" + split[1] + ") in argument: " + arg);
+                    printUsage();
+                    return;
+                }
+                if (port > 65535 || port < 0) {
+                    System.err.println("Port number out of range (0-65535): " + port);
+                    printUsage();
+                    return;
+                }
+                String organization = split[2];
 
-            nodes.add(new ClientNodeService(host, port, organization));
-        }
+                nodes.add(new ClientNodeService(host, port, organization));
+            }
 
-        CommandProcessor processor = new CommandProcessor(nodes);
-        processor.userInputLoop();
+            CommandProcessor processor = new CommandProcessor(nodes);
+            processor.userInputLoop();
+        
+        } finally {
+            terminateNodeChannels(nodes);
+        }     
+    }
 
-        for(ClientNodeService node : nodes){
+    private static void terminateNodeChannels(ArrayList<ClientNodeService> nodes) {
+        for (ClientNodeService node : nodes) {
             node.closeChannel();
         }
     }
