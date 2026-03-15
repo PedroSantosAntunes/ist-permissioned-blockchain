b27e4089c71d21b6ae3953f98d996069222c6001
Michael Maycock <michael.maycock@tecnico.ulisboa.pt>
2026-03-15 14:46:45 +0000

    Add new BlockRecord domain and proto classes

---
+++ common/src/main/java/pt/tecnico/blockchainist/block/BlockRecord.java
@@ -0,0 +1,29 @@
+package pt.tecnico.blockchainist.block;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import pt.tecnico.blockchainist.record.TransactionRecord;
+
+public class BlockRecord {
+    private final int blockNumber;
+    private final List<TransactionRecord> transactions;
+
+    public BlockRecord(int blockNumber, List<TransactionRecord> transactions) {
+        this.blockNumber = blockNumber;
+        this.transactions = new ArrayList<>(transactions);
+    }
+
+    public int getBlocknumber() {
+        return blockNumber;
+    }
+
+    public List<TransactionRecord> getTransactions() {
+        return Collections.unmodifiableList(transactions);
+    }
+
+    public int size() {
+        return transactions.size();
+    }
+}
